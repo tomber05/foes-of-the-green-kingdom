881973b40d5d7f9bba921a2d220f9a0f79c0fd13
Klagersnurfken <wokutomi@gmail.com>
2026-02-08 21:25:25 +0100

    Boton reinicio y ajustes de controles

---
+++ escenas/game.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _on_reinicio_pressed():
+	get_tree().reload_current_scene()
--- escenas/player.gd
+++ escenas/player.gd
@@ -27,7 +27,7 @@ func _physics_process(delta):
 	velocity += get_gravity() * delta
 	
 	#Salto
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Saltar") and is_on_floor():
 		velocity.y = _velocidad_salto
 	
 	#movimiento
